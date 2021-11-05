06dea456f9c57331d5ccae14ec88ad6ed28d21f4
OfalWoz <lsk.craft@gmail.com>
2021-11-05 09:51:33 +0100

    add ability to changing question onclick

--- app/src/main/java/pl/ofalwoz/quizapp/MainActivity.java
+++ app/src/main/java/pl/ofalwoz/quizapp/MainActivity.java
@@ -12,10 +12,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Question[] questions = new Question[]{
+            new Question(R.string.question1, true),
+            new Question(R.string.question2, true),
+            new Question(R.string.question3, true)
+    };
+
     public static final String Extra_MESSAGE = "pl.ofalwoz.quizapp.MESSAGE";
     private TextView textView;
     private TextView test;
+    private TextView question;
     public static final int TEXT_REQUEST = 1;
+
+    public int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.points);
         test = findViewById(R.id.textView);
+        question = findViewById(R.id.question);
+        question.setText(questions[i].getTextId());
     }
 
     public void backActivity(View view) {
+        if (i<=0)
+            i = 2;
+        else
+            i=i-1;
+        question.setText(questions[i].getTextId());
     }
 
     public void nextActivity(View view) {
+        if(i<2)
+            i++;
+        else
+            i = 0;
+        question.setText(questions[i].getTextId());
     }
 
     public void cheat(View view) {
---
+++ app/src/main/java/pl/ofalwoz/quizapp/Question.java
@@ -0,0 +1,20 @@
+package pl.ofalwoz.quizapp;
+
+public class Question {
+
+    private int textId;
+    private boolean answer;
+
+    public Question(int textId, boolean answer){
+        this.textId=textId;
+        this.answer=answer;
+    }
+
+    public int getTextId(){
+        return textId;
+    }
+
+    public boolean isAnswer(){
+        return answer;
+    }
+}
